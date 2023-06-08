b0aabcbe1d24ce2279c69aa8d73b2ffd4b1ff2fd
DjangoM <macej.jaroslav@protonmail.com>
2023-06-08 13:58:23 +0200

    added notes

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+
+  // enter the site
   await page.goto('https://www.ethereum.network/');
+
+  // click "Learn here"
   await page.goto('https://www.ethereum.network/faq/');
+  
   await page.getByRole('link', { name: 'BLOG' }).click();
   await page.getByRole('link', { name: 'BLOG' }).click();
   await page.getByRole('link', { name: 'EN Guides: #1 – Get started', exact: true }).click();
