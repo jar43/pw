e84f51a6fe60216abd04c15e662652c57b1faaa5
jar43 <macej.jaroslav@protonmail.com>
2023-07-12 11:38:43 +0200

    reorder

--- testy_1/5.@.spec.ts
+++ testy_1/5.@.spec.ts
@@ -3,34 +3,34 @@ import { test, expect } from '@playwright/test';
 test('kosik - prejst k platbe', async ({ page }) => {
   await page.goto('https://magento.softwaretestingboard.com/training.html');
 
-  // z rolovacieho menu vyber Men<Tops<Jackets
-  await page.getByRole('menuitem', { name: 'Men' }).hover();
-  await page.getByRole('menuitem', { name: 'Tops' }).hover();
-  await page.getByRole('menuitem', { name: 'Jackets' }).click();
-
+  // z rolovacieho menu vyber Men > Tops > Jackets
+  await page.hover('text="Men"');
+  await page.hover('text="Tops"');
+  await page.hover('text="Jackets"');
+  await page.click('text="Jackets"');
 
   // presmerovanie na stranku https://magento.softwaretestingboard.com/men/tops-men/jackets-men.html
 
   // stlac pole ponuky 'Montana Wind Jacket'
-  await page.getByRole('link', { name: 'Montana Wind Jacket' }).first().click();
+  await page.click('text="Montana Wind Jacket"');
 
   // vyber lubovolnu velkost, farbu
-  await page.getByRole('option', { name: 'S', exact: true }).click();
-  await page.getByRole('option', { name: 'Green' }).click();
+  await page.selectOption('select[name="super_attribute[92]"]', 'S');
+  await page.selectOption('select[name="super_attribute[93]"]', 'Green');
 
   // potvrd stlacenim 'Add to Cart'
-  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.click('text="Add to Cart"');
 
   // stlac tlacidlo kosik
-  await page.getByRole('link', { name: ' My Cart 1 1\nitems' }).click();
+  await page.click('text=" My Cart 1 1\nitems"');
 
   // stlac 'View and Edit Cart'
-  await page.getByRole('link', { name: 'View and Edit Cart' }).click();
+  await page.click('text="View and Edit Cart"');
 
   // presmerovanie na stranku 'https://magento.softwaretestingboard.com/checkout/cart/'
 
   // stlac 'Proceed to Checkout'
-  await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
+  await page.click('text="Proceed to Checkout"');
 
   // presmerovanie na stranku 'https://magento.softwaretestingboard.com/checkout/#shipping'
   await page.goto('https://magento.softwaretestingboard.com/checkout/#shipping');
