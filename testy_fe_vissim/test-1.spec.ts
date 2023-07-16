cd78dae071248515a842b6a6496d2f20a46467c1
jar43 <macej.jaroslav@protonmail.com>
2023-07-16 21:25:44 +0200

    preparations

--- testy_fe_vissim/test-1.spec.ts
+++ testy_fe_vissim/test-1.spec.ts
@@ -1,15 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.vissim.no/');
-  await page.getByRole('link', { name: 'Join us' }).click();
-  await page.getByRole('link', { name: 'See available positions' }).click();
-  await page.locator('#yui_3_17_2_1_1689503491914_1497').click();
-  await page.getByRole('link', { name: 'Kosice', exact: true }).click();
-  await page.locator('#yui_3_17_2_1_1689503491914_1501').click();
-  await page.getByRole('link', { name: 'vacancy' }).click();
-  await page.getByPlaceholder('Search', { exact: true }).click();
-  await page.getByPlaceholder('Search', { exact: true }).fill('testing');
-  await page.getByPlaceholder('Search', { exact: true }).press('Enter');
-  await page.locator('#yui_3_17_2_1_1689503491914_88').click();
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.vissim.no/career/#1');
+});
+
+test.describe('filtrovanie ponuk', () => {
+
+  test('location', async ({ page }) => {
+    await page.getByRole('link', { name: 'Join us' }).click();
+    await page.getByRole('link', { name: 'See available positions' }).click();
+    await page.locator('#yui_3_17_2_1_1689503491914_1497').click();
+    await page.getByRole('link', { name: 'Kosice', exact: true }).click();
+  });
+
+  test('type of listing', async ({ page }) => {
+    await page.locator('#yui_3_17_2_1_1689503491914_1501').click();
+    await page.getByRole('link', { name: 'vacancy' }).click();
+  });
+
+  test('search', async ({ page }) => {
+    await page.getByPlaceholder('Search', { exact: true }).click();
+    await page.getByPlaceholder('Search', { exact: true }).fill('testing');
+    await page.getByPlaceholder('Search', { exact: true }).press('Enter');
+    await page.locator('#yui_3_17_2_1_1689503491914_88').click();
+  });
 });
