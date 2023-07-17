ee1ad4de8094c25112a0c49d182f008b18a509cb
jar <35060411+jar43@users.noreply.github.com>
2023-07-17 17:47:43 +0200

    Update 3.spec.ts
    car edition 17.7.

--- testy_fe_vissim/3.spec.ts
+++ testy_fe_vissim/3.spec.ts
@@ -13,8 +13,25 @@ test.describe('Open Application', () => {
 
   test('', async ({ page }) => {
 
+    // skontroluj, ci jedna z PAP ma nazov 'Open Application'
     // skontroluj, ci filter 'TYPE OF LISTING' obsahuje moznost vyberu 'open application'
     // skontroluj, ci po pouziti filtra 'TYPE OF LISTING' a vybere 'open application' sa zobrazi iba PAP 'Open Application'
+
+
+    await page.goto('https://www.vissim.no/available-positions-1/open-application');
+
+    // skontroluj, ci klikom na obe tlacidla 'Apply now' ta presmeruje na formular
+    // skontroluj, ci formular ma url 'https://jobs.datacruit.com/en/reaction/open-application-vissim-no-172275'
+
+    // skontroluj, ci stranka obsahuje text 'send us an open application'
+
+
+    await page.goto('https://jobs.datacruit.com/en/reaction/open-application-vissim-no-172275');
+
+    // skontroluj, ci formular funguje..
+    - polia su prazdne, editovatelne, klikatelne
+    - povinne polia pri chybnom vyplneni zobrazia chybu, text
+    - ma funkcnu moznost 'Upload your CV'
 
 
   });
