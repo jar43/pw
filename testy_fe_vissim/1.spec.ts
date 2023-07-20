fa2f986c1e4c886714f9b5f31af6f37ab1c3184f
jar43 <macej.jaroslav@protonmail.com>
2023-07-20 14:12:11 +0200

    new edition

--- testy_fe_vissim/1.spec.ts
+++ testy_fe_vissim/1.spec.ts
@@ -12,30 +12,73 @@ test.beforeEach(async ({ page }) => {
 test.describe('filtrovanie poloziek ', () => {
 
   test('location - Kosice', async ({ page }) => {
-    // vytvorenie noveho lokatora
+
+    // skontroluj, ze existuje vo filtri PAP polozka >location>Kosice
+    const location_items = page.locator('#yui_3_17_2_1_1689842969548_785');
+    await expect.soft(page.location_items).toHaveText('Kosice');
+
+    // vyber filter >location>Kosice
     const role = page.locator('#yui_3_17_2_1_1689503491914_1497');
     const Kosice = page.locator('#yui_3_17_2_1_1689540127954_1558');
-    // vyber filter >location>Kosice
     await role.click();
     await Kosice.click();
 
     // skontroluj, ci pole filtra 'LOCATION' ma text 'LOCATION:Kosice'
-    await expect(page.locator('#yui_3_17_2_1_1689540141171_1528')).toHaveText('LOCATION:Kosice');
-    // skontroluj, ci vsetky vyfiltrovane PAP obsahuju text 'Kosice'
-    await expect(page.locator('#yui_3_17_2_1_1689540141171_88')).toContainText('Kosice');
+    const location = page.locator('#yui_3_17_2_1_1689540141171_1528');
+    await expect.soft(page.location).toHaveText('LOCATION:Kosice');
+
+    // skontroluj, ci vsetky vyfiltrovane PAP maju v nazve slovo 'Kosice'
+    const pap = page.locator('#yui_3_17_2_1_1689540141171_88');
+    await expect.soft(page.pap).toContainText('Kosice');
   });
 
   test('type of listing', async ({ page }) => {
+
+    // skontroluj, ci default hodnota filtra 'type of listing' je 'All'
+    const type_of_listing = page.locator('#yui_3_17_2_1_1689842969548_1712');
+    await expect(type_of_listing).toHaveValues([':All']);
+
+    // z filtra 'type of listing' vyber moznost 'kosice'
     await page.locator('#yui_3_17_2_1_1689503491914_1501').click();
-    await page.getByRole('link', { name: 'vacancy' }).click();
+    await page.getByRole('link', { name: 'kosice' }).click();
+
+    // skontroluj, ci vysledkom vyhladavania je zobrazenie PAP
+
+  });
+
+  test('location + type of listing', async ({ page }) => {
+
+    // vyber filter >location>Kosice
+    const role = page.locator('#yui_3_17_2_1_1689503491914_1497');
+    const Kosice = page.locator('#yui_3_17_2_1_1689540127954_1558');
+    await role.click();
+    await Kosice.click();
+
+    // z filtra 'type of listing' vyber moznost 'kosice'
+    await page.locator('#yui_3_17_2_1_1689503491914_1501').click();
+    await page.getByRole('link', { name: 'kosice' }).click();
+
+    // skontroluj, ci vysledok vyhladavania je rovnaky ako v testoch 'location' a 'type of listing'
+
   });
 
   test('search', async ({ page }) => {
+
+    // skontroluj, ci pole search je prazdne
+    const search = page.getByPlaceholder('Search', { exact: true });
+    await expect.soft(search).toBeEmpty();
+
+    // otestuj vyhladavanie pre slovo 'testing'
     await page.getByPlaceholder('Search', { exact: true }).click();
     await page.getByPlaceholder('Search', { exact: true }).fill('testing');
     await page.getByPlaceholder('Search', { exact: true }).press('Enter');
-    await page.locator('#yui_3_17_2_1_1689503491914_88').click();
 
-    // skontroluj, ci pole search je prazdne
+    // skontroluj, ci po uspesnom vyhladavani sa zobrazia PAP
+
+    // skontroluj, ci po neuspesnom vyhladavani sa zobrazi text s oznamom
+    const search_message = page.locator('#yui_3_17_2_1_1689503491914_88');
+    await expect.soft(search_message).toHaveText('.*');
+
   });
+
 });
--- testy_fe_vissim/3.spec.ts
+++ testy_fe_vissim/3.spec.ts
@@ -1,9 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.vissim.no/career#1');
-});
-
 /**
  * pouzite skratky:
  * PAP = polozka zo sekcie 'Avalaible Positions'
@@ -11,12 +7,32 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Open Application', () => {
 
-  test('', async ({ page }) => {
-
+  test('Open Application - PAP a filter', async ({ page }) => {
+    await page.goto('https://www.vissim.no/career#1');
+  
     // skontroluj, ci jedna z PAP ma nazov 'Open Application'
-    // skontroluj, ci filter 'TYPE OF LISTING' obsahuje moznost vyberu 'open application'
-    // skontroluj, ci po pouziti filtra 'TYPE OF LISTING' a vybere 'open application' sa zobrazi iba PAP 'Open Application'
+    await expect(page.locator('#block-6f83cad10c9b33fc06b4')).toHaveId('#yui_3_17_2_1_1689664139257_1663');
 
+    // skontroluj, ci filter 'TYPE OF LISTING' obsahuje moznost vyberu 'open application'
+    // Get the list of items on the page.
+    const items = await page.locator('#items').querySelectorAll('li');
+    // Filter the items by text.
+    const filteredItems = await items.filter((item) => item.textContent.includes('The'));
+    // Assert that the filtered items contain the expected text.
+    for (const item of filteredItems) {
+    expect(item.textContent).toContain('');
+
+    // skontroluj, ci po pouziti filtra 'TYPE OF LISTING' a vybere 'open application' sa zobrazi iba PAP 'Open Application'
+    await page.locator('#yui_3_17_2_1_1689714840287_1620').click();
+    await page.getByRole('link', { name: 'open application', exact: true }).click();
+    const list = page.locator('list > .component');
+    await expect(list).toHaveCount(3); 
+    const locator = page.locator('.title');
+    await expect(locator).toHaveText(/Welcome, Test User/);
+    await expect(locator).toHaveText(/Welcome, .*/);
+  });
+
+  test('Open Application - detail', async ({ page }) => {
 
     await page.goto('https://www.vissim.no/available-positions-1/open-application');
 
@@ -24,14 +40,16 @@ test.describe('Open Application', () => {
     // skontroluj, ci formular ma url 'https://jobs.datacruit.com/en/reaction/open-application-vissim-no-172275'
 
     // skontroluj, ci stranka obsahuje text 'send us an open application'
+  });
 
+    test('Open Application - formular', async ({ page }) => {
 
     await page.goto('https://jobs.datacruit.com/en/reaction/open-application-vissim-no-172275');
 
     // skontroluj, ci formular funguje..
-    - polia su prazdne, editovatelne, klikatelne
-    - povinne polia pri chybnom vyplneni zobrazia chybu, text
-    - ma funkcnu moznost 'Upload your CV'
+    // - polia su prazdne, editovatelne, klikatelne
+    // - povinne polia pri chybnom vyplneni zobrazia chybu, text
+    // - ma funkcnu moznost 'Upload your CV'
 
 
   });
