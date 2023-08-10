e453feb1f3b5ed70d665a820a683f21536323681
jar43 <macej.jaroslav@protonmail.com>
2023-08-10 11:02:54 +0200

    new edition

--- testy_fe_vissim/1.1.spec.ts
+++ testy_fe_vissim/1.1.spec.ts
@@ -22,9 +22,21 @@ test.describe('filtrovanie poloziek ', () => {
     const papDataTitle = 'QA Automation Engineer for new hub (wind energy) - Kosice (Slovakia)' // nahrad titulkom vybranej vyfiltrovanej PAP
     const papFiltered = await page.waitForSelector('a:has-text("${papDataTitle}")', {state: 'visible'});
     await expect(papFiltered).toContain('Kosice');
-
-  
-
-
   });
+
+  test('pouzitie filtra - type of listing', async ({ page }) => {
+
+    // vyber hodnotu filtra - type of listing
+    const hrefFilterValue = "/available-positions-1?type-of-listing=vacancy"; // nahrad vybranou hodnotou filtra
+    const filterValue = await page.waitForSelector('a[href="${hrefFilterValue}"]', {state: 'visible'});
+    await filterValue.click();
+
+    // skontroluj, ci vyfiltrovane PAP neobsahuju PAP s nazvom Open Application
+    const papDataTitle = 'Open Application' // nahrad titulkom vybranej vyfiltrovanej PAP
+    const papFiltered = await page.waitForSelector('a:has-text("${papDataTitle}")', {state: 'visible'});
+    await expect(papFiltered).not.toContain('Open Application');
+
+    // skontroluj, ze pocet vyfiltrovanych PAP = n-1; ked n = celkovy pocet PAP bez pouzitia filtra
+  });
+
 });
